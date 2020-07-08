caf762b1a961488c5d01e9578a4cf42e5df05824
Dreae <dreae@dreae.onl>
2020-07-07 23:26:31 -0400

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Loggregate.MixProject do
   def project do
     [
       app: :loggregate,
-      version: "0.2.4",
+      version: "0.3.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
