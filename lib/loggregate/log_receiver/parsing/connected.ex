80d196735a74a7288549f6c093fd175eaa049ed9
Dreae <dreae@dreae.onl>
2020-07-17 21:47:30 -0400

    Refactor location lookup into macro

--- lib/loggregate/log_receiver/parsing/connected.ex
+++ lib/loggregate/log_receiver/parsing/connected.ex
@@ -2,18 +2,14 @@ defmodule Loggregate.LogReceiver.Parsers.Connected do
   @behaviour Loggregate.LogReceiver.Parser
 
   require Logger
+  require Loggregate.LogReceiver.Parser
+  alias Loggregate.LogReceiver.Parser
 
   @connected_regex ~r/^"(.+)<\d+><([\S]+)><>" connected, address "([0-9.]+):([0-9]+)"$/
   def parse(message) do
     if Regex.match?(@connected_regex, message) do
       [_, name, steamid, address, port] = Regex.run(@connected_regex, message)
-      loc = case :locus.lookup(:maxmind, address) do
-        {:ok, %{"latitude" => lat, "longitude" => lon}} = _entry -> %{lat: lat, lon: lon}
-        err ->
-          Logger.warn("Error getting location data #{inspect(err)}")
-
-          nil
-      end
+      loc = Parser.lookup_location(address)
 
       {:ok, %{line: message, type: :connected, who: %{steamid: steamid, name: name, name_kw: name, address: address, port: port, location: loc}}}
     else
--- lib/loggregate/log_receiver/parsing/rcon.ex
+++ lib/loggregate/log_receiver/parsing/rcon.ex
@@ -2,18 +2,14 @@ defmodule Loggregate.LogReceiver.Parsers.Rcon do
   @behaviour Loggregate.LogReceiver.Parser
 
   require Logger
+  require Loggregate.LogReceiver.Parser
+  alias Loggregate.LogReceiver.Parser
 
   @rcon_regex ~r/^rcon from "((?:[0-9]{1,3}\.){3}[0-9]{1,3}):([0-9]{1,5})": command "(.*)"$/
   def parse(message) do
     if Regex.match?(@rcon_regex, message) do
       [_, address, port, command] = Regex.run(@rcon_regex, message)
-      loc = case :locus.lookup(:maxmind, address) do
-        {:ok, %{"latitude" => lat, "longitude" => lon}} = _entry -> %{lat: lat, lon: lon}
-        err ->
-          Logger.warn("Error getting location data #{inspect(err)}")
-
-          nil
-      end
+      loc = Parser.lookup_location(address)
 
       {:ok, %{line: message, type: :rcon, command: command, from_addr: %{address: address, port: port, location: loc}}}
     else
